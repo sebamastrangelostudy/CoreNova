00e147ce7354b70cba6c16c0075b4b356a8dafa5
SebastianM <sebamstudyprogrammer@gmail.com>
2026-04-08 23:55:53 -0300

    add link to product catalog in product cards

--- CoreNova/JavaScript/index.js
+++ CoreNova/JavaScript/index.js
@@ -21,6 +21,7 @@ fetch("products.json")
         <img src="${component.image}" alt="${component.brand} ${component.model}"class="catalog__card-img" />
         <h3 class="catalog__card-title">${component.brand} ${component.model}
         </h3>
+        <a href="../CoreNova/pages/catalogo.html" class="catalog__card-btn">Ver más</a>
       `;
 
       componentProdI.appendChild(card);
